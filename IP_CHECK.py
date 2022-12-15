e15992320b0fa9bb93cab050c1af36ac20bf35b7
sraspi <77743358+sraspi@users.noreply.github.com>
2022-12-15 10:42:54 +0100

    update

--- IP_CHECK.py
+++ IP_CHECK.py
@@ -3,6 +3,14 @@ import time
 import ip_mail
 import os
 
+
+#s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#s.connect(("8.8.8.8", 80))
+#print(s.getsockname()[0])
+
+#print(socket.gethostbyname(socket.gethostname()))
+
+
 time.sleep(30)
 IPA0 = os.popen('hostname -I').readline(15)
 print(IPA0)
@@ -13,14 +21,19 @@ ip_mail.ip_mail()
 
 
 while True:
-    IPA0 = os.popen('hostname -I').readline(15)
-    time.sleep(10)
+    time.sleep(600)
     IPA1 = os.popen('hostname -I').readline(15)
     
+    
     if IPA0 != IPA1:
+        IPA1 = os.popen('hostname -I').readline(15)
         f = open("/home/pi/ip.txt", "w")
         f.write(str(IPA1))
         f.close()
+        IPA0 = IPA1
         ip_mail.ip_mail()
+        
+    
+        
         
     
